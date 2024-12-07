479935a23be70f0dc3014ce64d63148a513e0b6e
Aditya rai <raijiaditya9868@gmail.com>
2024-12-08 01:37:17 +0530

    m

--- src/components/Envelope.tsx
+++ src/components/Envelope.tsx
@@ -27,27 +27,33 @@ export function Envelope({ onOpen, ...props }: EnvelopeProps) {
   return (
     <div className="relative w-[400px] perspective-1000">
       <div 
-        className={`transition-all duration-1000 transform-style-3d ${
+        className={`relative transition-all duration-1000 transform-style-3d ${
           isOpen ? 'translate-y-[-100px]' : ''
         } ${isHidden ? 'opacity-0 translate-y-[-200px]' : ''}`}
       >
+        {/* Wedding Details Card - Only show when envelope is open */}
+        <div className="absolute inset-0 z-10">
+          {isOpen && (
+            <WeddingDetails
+              isRevealed={isHidden}
+              details={props}
+            />
+          )}
+        </div>
+
         {/* Envelope Back with Flap */}
-        <EnvelopeBack isOpen={isOpen} onClick={handleEnvelopeClick} />
+        <div className="relative z-20">
+          <EnvelopeBack isOpen={isOpen} onClick={handleEnvelopeClick} />
+        </div>
 
         {/* Envelope Front */}
-        <EnvelopeFront onClick={handleEnvelopeClick}>
-          <Heart className="w-8 h-8 text-[#b8860b]" />
-          <p className="text-lg font-serif text-[#704214] mt-4">Wedding Invitation</p>
-          <p className="text-sm text-[#8b4513] mt-2 italic">Click to open</p>
-        </EnvelopeFront>
-
-        {/* Wedding Details Card - Only show when envelope is open */}
-        {isOpen && (
-          <WeddingDetails
-            isRevealed={isHidden}
-            details={props}
-          />
-        )}
+        <div className="relative z-30">
+          <EnvelopeFront onClick={handleEnvelopeClick}>
+            <Heart className="w-8 h-8 text-[#b8860b]" />
+            <p className="text-lg font-serif text-[#704214] mt-4">Wedding Invitation</p>
+            <p className="text-sm text-[#8b4513] mt-2 italic">Click to open</p>
+          </EnvelopeFront>
+        </div>
       </div>
     </div>
   );
